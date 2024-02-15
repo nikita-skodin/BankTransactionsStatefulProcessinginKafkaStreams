dc40df498b0bb508cbdf3c5f3e9848e5e9a06e5e
nikita-skodin <skodin36@gmail.com>
2024-02-15 13:40:22 +0300

    Initial commit

---
+++ src/main/java/com/github/programmingwithmati/model/BankBalance.java
@@ -0,0 +1,35 @@
+package com.github.programmingwithmati.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BankBalance {
+
+    private Long id;
+    private BigDecimal amount = BigDecimal.ZERO;
+    private Date lastUpdate;
+    private BankTransaction latestTransaction;
+
+    public BankBalance process(BankTransaction bankTransaction) {
+
+        this.id = bankTransaction.getBalanceId();
+        this.lastUpdate = bankTransaction.getTime();
+        this.latestTransaction = bankTransaction;
+
+        if (amount.add(bankTransaction.getAmount()).compareTo(BigDecimal.ZERO) < 0){
+            bankTransaction.setState(BankTransaction.BankTransactionState.REJECTED);
+        } else {
+            bankTransaction.setState(BankTransaction.BankTransactionState.APPROVED);
+            amount = amount.add(bankTransaction.getAmount());
+        }
+
+        return this;
+    }
+}
---
+++ src/main/java/com/github/programmingwithmati/topology/BankBalanceTopology.java
@@ -0,0 +1,45 @@
+package com.github.programmingwithmati.topology;
+
+import com.github.programmingwithmati.model.BankBalance;
+import com.github.programmingwithmati.model.BankTransaction;
+import com.github.programmingwithmati.model.JsonSerde;
+import org.apache.kafka.common.serialization.Serdes.LongSerde;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
+
+public class BankBalanceTopology {
+
+    public static final String BANK_TRANSACTIONS = "bank-transactions";
+    public static final String BANK_BALANCES = "bank-balances";
+    public static final String REJECTED_TRANSACTIONS = "rejected-transactions";
+
+    private static final LongSerde LONG_SERDE = new LongSerde();
+    private static final JsonSerde<BankBalance> BANK_BALANCE_SERDE = new JsonSerde<>(BankBalance.class);
+    private static final JsonSerde<BankTransaction> BANK_TRANSACTION_SERDE = new JsonSerde<>(BankTransaction.class);
+
+    public static Topology buildTopology() {
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        var bankBalanceKStream = streamsBuilder.stream(BANK_TRANSACTIONS, Consumed.with(LONG_SERDE, BANK_TRANSACTION_SERDE))
+                .groupByKey()
+                .aggregate(BankBalance::new, (key, value, aggregate) -> aggregate.process(value),
+                        Materialized.with(LONG_SERDE, BANK_BALANCE_SERDE))
+                .toStream();
+
+        bankBalanceKStream.to(BANK_BALANCES);
+
+        bankBalanceKStream
+                .mapValues(BankBalance::getLatestTransaction)
+                .filter((key, value) ->
+                        value.getState().equals(BankTransaction.BankTransactionState.REJECTED))
+                .to(REJECTED_TRANSACTIONS, Produced.with(LONG_SERDE, BANK_TRANSACTION_SERDE));
+
+        Topology build = streamsBuilder.build();
+
+        System.err.println(build.describe());
+        return build;
+    }
+}
